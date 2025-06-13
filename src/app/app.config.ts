a7da6316b9d4df3989a62b9faa4330a65365997e
Mirze1993 <58223139+Mirze1993@users.noreply.github.com>
2025-06-13 19:33:35 +0400

    c

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,40 +8,32 @@ import {registerLocaleData} from '@angular/common';
 import en from '@angular/common/locales/en';
 import {FormsModule} from '@angular/forms';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withFetch} from '@angular/common/http';
 import {httpInterceptorProviders, HttpRequestInterceptor} from './helper/http-request-interceptor.service';
 import {NZ_ICONS} from 'ng-zorro-antd/icon';
 import {icons} from './icons';
 import {environment} from '../environments/environment';
-import {persistState} from '@datorama/akita';
 
 registerLocaleData(en);
 
 const baseUrl = typeof window !== 'undefined'
   ? `${location.protocol}//${location.hostname}:${environment.port}`
-  : 'http://localhost:4200';
+  : 'http://localhost:4200'+environment.port;
 
 
-let akitaPersistStorage: any = undefined;
-if (typeof window !== 'undefined' && typeof localStorage !== 'undefined') {
-  akitaPersistStorage = persistState({
-    key: 'AkitaStores',
-    include: ['auth']
-  });
-}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideNzI18n(en_US), importProvidersFrom(FormsModule),
     provideAnimationsAsync(),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     { provide: NZ_ICONS, useValue: icons },
     httpInterceptorProviders,
     {
       provide: 'baseUrl',
       useValue: baseUrl
-    },
-    ...(akitaPersistStorage ? [{ provide: 'persistStorage', useValue: akitaPersistStorage }] : [])
+    }
   ]
 };
