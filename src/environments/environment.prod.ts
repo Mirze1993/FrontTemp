baa3dcf6ae7fbb372dcace7a01ef24a382700c37
MirzaRabita <m.c.abbasaliyev@gmail.com>
2025-09-24 15:14:44 +0400

    add docker file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 import {EnvironmentModel} from './environment-model';
 
 export const environment: EnvironmentModel = {
-  production: false,
-  authApiUrl: 'http://localhost:5197' ,
-  aiApiUrl: 'http://localhost:5007' ,
-  fileUrl:"http://localhost:5163",
+  production: true,
+  authApiUrl: 'https://auth.mc-blog.space' ,
+  aiApiUrl: 'https://ai.mc-blog.space' ,
+  fileUrl:"https://file.mc-blog.space",
 };
